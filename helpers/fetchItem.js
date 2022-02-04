a3f6ea631bd0593f325d2984ecc5d527d21e1e5f
PatriciaDivi <patriciavianad@gmail.com>
2022-02-04 16:32:40 -0300

    2 Requisito | Adicione o produto ao carrinho de compras | :rocket:

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,13 @@
-const fetchItem = () => {
+const fetchItem = async (idProduct) => {
   // seu código aqui
+  const END_POINT = `https://api.mercadolibre.com/items/${idProduct}`;
+  const response = await fetch(END_POINT);
+  const resultsItem = await response.json();
+  // console.log(resultsItem);
+  return resultsItem;
 };
+// fetchItem('MLB1607748387');
+// fetchItem('MLB1607748387').then(console.log);
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const showCardOl = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,25 +7,56 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function showCard(resId) {
+  const idItem = await fetchItem(resId);
+  const { id, title, price } = idItem;
+  // console.log(title);
+  showCardOl.appendChild(createCartItemElement({ id, title, price }));
+
+  // console.log(idItem);
+  // console.log(idItem.id);
+  // console.log(Object.assign([idItem.id, idItem.title, idItem.title]));
+  
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function clickButtom(event) {
+  // REF = https://www.w3schools.com/jsref/prop_node_firstchild.asp
+  const element = event.target;
+  if (element.classList.contains('item__add')) {
+    const id = getSkuFromProductItem(element.parentNode);
+    // const id = element.parentElement.firstElementChild.innerText;
+    // const id = element.parentElement.childNodes[0].innerText;
+    console.log(id);
+    return showCard(id);
+  }
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
-  // e.addEventListener('click', clickButtom);
+  e.addEventListener('click', clickButtom);
   return e;
 }
-
-// function clickButtom() {
-//   return alert('Foi');
-// }
-// clickButtom();
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) { // alias {
   const section = document.createElement('section');
   section.className = 'item';
   const newImageQuality = image.split('-');
   const imageQuality = [...newImageQuality[0], '-', ...newImageQuality[1], '-J.jpg'].join('');
-  console.log(imageQuality);
+  // console.log(imageQuality);
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(imageQuality));
@@ -42,20 +75,9 @@ async function createShoppingCard() {
 }
 createShoppingCard();
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function cartItemClickListener(event) {
+  const element = event.target;
+  showCardOl.removeChild(element);
+}
 
 window.onload = () => { };
