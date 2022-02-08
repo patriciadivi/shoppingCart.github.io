03a980b7cfb7162361cdc05c5f7b1e9bdb480159
PatriciaDivi <patriciavianad@gmail.com>
2022-02-08 00:49:14 -0300

    11 Requisito | Desenvolva testes para atingir 100% de cobertura total e 100% da função getSavedCartItems | :rocket:

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -6,5 +6,13 @@ localStorageSimulator('getItem');
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
   // fail('Teste vazio');
-  it('descrição', () => {});
+  it('I - Teste se, ao executar `getSavedCartItems`, o método `localStorage.getItem` é chamado;', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled()
+  });
+
+  it('II - Teste se, ao executar `getSavedCartItems`, o método `localStorage.getItem` é chamado com o "cartItems" como parâmetro.',() => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  })
 });
